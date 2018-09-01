f41dcdc7b743971133b0cb2bccd73446e187ea35
alextaoultsides <alextaoultsides@gmail.com>
2018-09-01 18:29:00 -0400

    First Commit

---
+++ Nineties Fortune Ball/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Nineties Fortune Ball
+//
+//  Created by atao1 on 9/1/18.
+//  Copyright © 2018 atao. All rights reserved.
+//
+
+import UIKit
+
+class FortuneBallViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# 90's Fortune Ball
+
+Ask a question and see what your fortune holds... kinda.
+
+Accuracy is not guaranteed.
